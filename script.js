9059d2b3d0a6dbdbe67a0f873753429ee374edbd
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-11-04 14:34:24 +0200

    task 8

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ console.log('Task 6')
 const str = 'my-short-string'
 let arr = str.split("-")
 console.log(arr)
+
+console.log('Task 7')
+const arr2 = ['JavaScript', 2015]
+console.log(arr2.join(' '))
+
+console.log('Task 8')
+let users = 
+[
+    {id: 1, name: "Vic", age: 21},  
+    {id: 2, name: "Petya", age: 30}, 
+    {id: 3, name: "Jon", age: 5},
+
+]
+
+const fltr = users.filter(item => item.age > 20)
+console.log(fltr)
+
+console.log('Task 9')
