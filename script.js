4e6318de561f6c2d5cfb8ef2416e36598694290d
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-11-04 13:30:49 +0200

    task 5

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+console.log('Task 5')
 
+const ask = (question, yes, no) => {
+    if(confirm(question)) yes()
+        else no()
+}
+
+ask('Вы согласны?', function () {
+    console.log('Вы согласились')
+}, function () {
+    console.log('Вы отменили выполнение.')
+})
+
+console.log('Task 6')
