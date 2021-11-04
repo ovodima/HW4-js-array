fce87f19454fa5a7a4a378431143095c03e541d1
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-11-04 14:26:11 +0200

    task6

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 console.log('Task 5')
 
-const ask = (question, yes, no) => {
-    if(confirm(question)) yes()
-        else no()
-}
+// const ask = (question, yes, no) => {
+//     confirm(question) ? yes() : no()
+// }
 
-ask('Вы согласны?', () => {
-    console.log('Вы согласились')
-},  () => {
-    console.log('Вы отменили выполнение.')
-})
+// ask('Вы согласны?', () => {
+//     console.log('Вы согласились')
+// },  () => {
+//     console.log('Вы отменили выполнение.')
+// })
 
 console.log('Task 6')
+
+const str = 'my-short-string'
+let arr = str.split("-")
+console.log(arr)
